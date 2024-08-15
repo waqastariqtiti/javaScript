bdeca498f50ef1863e72920d527725bbb237cef5
waqas <tariqwaqasttii@gmail.com>
2024-08-15 20:14:37 +0500

    add functions

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("waqas");
+function typeOfValue(giveValue) {
+    return typeof giveValue
+}
+
+console.log(typeOfValue("saad"));
+function convertTypes(value) {
+    let num = Number(value);
+    let str = String(value);
+    let bool = Boolean(value);
+    
+    return { num, str, bool };
+}
+
+console.log(convertTypes("123")); 
+console.log(convertTypes("abc"));
